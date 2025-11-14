14a6ce4caee6f29524a8a6065fdd3f93b98df026
Vinay2080 <shaking.121@gmail.com>
2025-11-14 11:34:47 +0530

    setting up a project restaurant. added application.yaml and env file also pom.xml

---
+++ src/main/java/org/miniproject/restuarant/ProjectRestuarantApplication.java
@@ -0,0 +1,13 @@
+package org.miniproject.restuarant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestuarantApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ProjectRestuarantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miniproject/restuarant/ProjectRestuarantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miniproject.restuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRestuarantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
